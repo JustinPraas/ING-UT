fcf844e421dae2c90542f0f116f4cbee6db8383c
cjcr.andrei <cjcr.andrei@gmail.com>
2017-03-03 16:41:17 +0100

    Add IllegalTransferException
    The system should not allow transfers to be made if the source account
    does not have the balance to do so.

---
+++ ING Research/src/exceptions/IllegalTransferException.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+/**
+ * Thrown when a transfer is attempted with a source <code>BankAccount</code> that does not have 
+ * a sufficient balance to cover the chosen transfer amount.
+ * @author Andrei Cojocaru
+ */
+public class IllegalTransferException extends Exception {
+	float balance;
+	String IBAN;
+	float amount;
+	
+	public IllegalTransferException(float balance, String IBAN, float amount) {
+		this.balance = balance;
+		this.IBAN = IBAN;
+		this.amount = amount;
+	}
+	
+	public String toString() {
+		return "Attempted to transfer sum of " + amount + " from account " + IBAN + " which has a balance of " + balance;
+	}
+}
