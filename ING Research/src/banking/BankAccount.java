8c1de8cd5a78d2861b54e00bd1c5e797577eced4
cjcr.andrei <cjcr.andrei@gmail.com>
2017-03-03 17:34:06 +0100

    Change BankAccount constructor
    Moved the responsibility of linking newly created BankAccounts to the
    CustomerAccount method.

--- ING Research/src/banking/BankAccount.java
+++ ING Research/src/banking/BankAccount.java
@@ -26,7 +26,6 @@ public class BankAccount {
 		this.mainHolder = holder;
 		this.holders = new HashSet<CustomerAccount>();
 		this.holders.add(holder);
-		holder.getBankAccounts().add(this);
 	}
 	
 	/**
