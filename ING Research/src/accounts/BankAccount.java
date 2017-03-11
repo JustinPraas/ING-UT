5fceba06c35100941951d5ae7fab80adc44fe4d4
JustinPraas <j.w.praas@student.utwente.nl>
2017-03-11 15:15:10 +0100

    Implement a simple IBAN generator with contronl number calculations
    Add final static String COUNTRY_CODE = "NL" (BankAccount.java)
    Add final static String BANK_CODE = "INGB" (BankAccount.java)
    Implement the generateIBAN method (BankAccount.java)
    Implement the generateControlNumber method (BankAccount.java)
    Add a getter for the IBAN (BankAccount.java)

--- ING Research/src/accounts/BankAccount.java
+++ ING Research/src/accounts/BankAccount.java
@@ -1,6 +1,7 @@
 package accounts;
 
 import java.util.HashSet;
+import java.math.BigInteger;
 
 import exceptions.IllegalAmountException;
 import exceptions.IllegalCloseException;
@@ -11,6 +12,9 @@ import exceptions.IllegalTransferException;
  * @author Andrei Cojocaru
  */
 public class BankAccount {
+	private final static String COUNTRY_CODE = "NL";
+	private final static String BANK_CODE = "INGB";
+	
 	private float balance;
 	private String IBAN;
 	private CustomerAccount mainHolder;
@@ -22,9 +26,79 @@ public class BankAccount {
 	 */
 	public BankAccount(CustomerAccount holder) {
 		this.balance = 0;
+		this.IBAN = generateIBAN(COUNTRY_CODE, BANK_CODE);
 		this.mainHolder = holder;
 		this.holders = new HashSet<CustomerAccount>();
 		this.holders.add(holder);
+	}
+	
+	/**
+	 * Generates an IBAN for this BankAccount
+	 * @param countryCode The code that represents a country (e.g. NL, BE, DE, etc.)
+	 * @param bankCode The code that represents the Bank (e.g. INGB, ABNA, etc.)
+	 * @return resultIBAN The IBAN 
+	 */
+	public static String generateIBAN(String countryCode, String bankCode) {
+		//The personalAccountNumber (last 10 digits) should be hierarchically distributed.
+		//However, we do not keep track of used numbers yet, so for now assign 10 random digits
+		StringBuilder personalAccountNumber = new StringBuilder();
+		for (int i = 0; i < 10; i++) {
+			personalAccountNumber.append((int) (Math.random() * 10));
+		}		
+		
+		//Compute the controlNumber for this IBAN
+		int controlNumber = generateControlNumber(countryCode, bankCode, personalAccountNumber.toString());
+		
+		//If the control number consists of 1 digit, prepend a 0
+		String controlNumberString = controlNumber < 10 ? "0" + controlNumber : "" + controlNumber;	
+		
+		//Concatenate all parts of the IBAN to a complete IBAN	
+		String resultIBAN = countryCode + controlNumberString + bankCode + personalAccountNumber;
+		
+		return resultIBAN;
+	}
+	
+	/**
+	 * Generates the controlNumber for the IBAN (3rd and 4th digit).
+	 * @param countryCode The code that represents a country (e.g. NL, BE, DE, etc.)
+	 * @param bankCode The code that represents the Bank (e.g. INGB, ABNA, etc.)
+	 * @param pan The personal account number (the last 10 digits)
+	 * @return controlNumber The controlNumber that belongs to the combination of 
+	 * countryCode, BankCode and pan
+	 * @see <a href="http://www.ibannl.org/uitleg-over-iban/">Formula for the control number (Dutch)</a>
+	 */
+	public static int generateControlNumber(String countryCode, String bankCode, String pan) {
+		/* 
+		 * Formula in short:
+		 * String a = bank code converted to digits (e.g. 'A' = 10, 'B' = 11, 'Z' = 35, etc.)
+		 * String b = personal account number (10 digits, prepend with 0's if needed)
+		 * String c = country code converted to digits (see String a)
+		 * BigInteger value = parse the following to a BigInteger: (a + b + c)
+		 * int controlNumber = 98 - (value % 97)
+		 */
+		
+		//Convert the bankCode to digits
+		StringBuilder bankCodeInDigits = new StringBuilder();
+		for (Character c : bankCode.toCharArray()) {
+			bankCodeInDigits.append(Character.getNumericValue(c));
+		} 
+		
+		//Convert the countryCode to digits
+		StringBuilder countryCodeInDigits = new StringBuilder();
+		for (Character c : countryCode.toCharArray()) {
+			countryCodeInDigits.append(Character.getNumericValue(c));
+		}
+		
+		//A BigInteger is necessary for a value of that does not fit a long
+		BigInteger computationValue = new BigInteger(bankCodeInDigits + pan + countryCodeInDigits + "00");
+		
+		//Get the remainder of computationValue % 97
+		computationValue = computationValue.mod(new BigInteger("97"));
+		
+		//Subtract the computationValue from 98, this results in the controlNumber of the debitcard
+		int controlNumber = 98 - computationValue.intValue();
+		
+		return controlNumber;
 	}
 	
 	/**
@@ -107,5 +181,9 @@ public class BankAccount {
 			throw new IllegalAmountException(amount);
 		}
 		balance += amount;
+	}
+	
+	public String getIBAN() {
+		return IBAN;
 	}
 }
