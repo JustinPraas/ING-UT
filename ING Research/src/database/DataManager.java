12e8fc1a6696f9210d1b6ed02698d4cde1430c5b
cjcr.andrei <cjcr.andrei@gmail.com>
2017-04-02 12:38:02 +0200

    Add primary key uniqueness test method in DataManager
    This is necessary because we often generate IBANs/card numbers which we
    need to guarantee are not duplicate.

--- ING Research/src/database/DataManager.java
+++ ING Research/src/database/DataManager.java
@@ -114,25 +114,35 @@ public class DataManager {
     	return results;
     }
     
-//    public static void main(String[] args) {
-//    	CustomerAccount john = new CustomerAccount("John", "Test", "TEST", "103 Testings Ave.", "000-TEST", "johntest@testing.test", "TESTDATE");
-//    	CustomerAccount jane = new CustomerAccount("Jane", "Test", "TEST2", "104 Testings Ave.", "001-TEST", "janetest@testing.test", "TESTDATE2");
-//    	BankAccount bAcc = new BankAccount("TEST", 100, "TESTIBAN");
-//    	BankAccount bAcc2 = new BankAccount("TEST2", 100, "TESTIBAN2");
-//    	DebitCard card = new DebitCard("TEST", "TESTIBAN");
-//    	john.saveToDB();
-//    	jane.saveToDB();
-//    	card.saveToDB();
-//    	jane.addBankAccount(bAcc);
-//    	john.addBankAccount(bAcc);
-//    	jane.addBankAccount(bAcc2);
-//    	save(john);
-//    	save(jane);
-//    	System.out.println(bAcc.getDebitCards().size());
-//    	System.out.println(bAcc2.getDebitCards().size());
-//    	john.deleteFromDB();
-//    	jane.deleteFromDB();
-//    	System.out.println(bAcc.getDebitCards().size());
-//    	System.out.println(bAcc2.getDebitCards().size());
-//    }
+    public static boolean isPrimaryKeyUnique(String className, String primaryKeyName, String primaryKey) {
+    	initIfRequired();
+    	
+    	Session session = factory.openSession();
+    	Criteria cr = session.createCriteria(className);
+    	cr.add(Restrictions.eq(primaryKeyName, primaryKey));
+    	
+    	return true;
+    }
+    
+    public static void main(String[] args) {
+    	CustomerAccount john = new CustomerAccount("John", "Test", "TEST", "103 Testings Ave.", "000-TEST", "johntest@testing.test", "TESTDATE");
+    	CustomerAccount jane = new CustomerAccount("Jane", "Test", "TEST2", "104 Testings Ave.", "001-TEST", "janetest@testing.test", "TESTDATE2");
+    	BankAccount bAcc = new BankAccount("TEST", 100, "TESTIBAN");
+    	BankAccount bAcc2 = new BankAccount("TEST2", 100, "TESTIBAN2");
+    	DebitCard card = new DebitCard("TEST", "TESTIBAN");
+    	john.saveToDB();
+    	jane.saveToDB();
+    	card.saveToDB();
+    	jane.addBankAccount(bAcc);
+    	john.addBankAccount(bAcc);
+    	jane.addBankAccount(bAcc2);
+    	save(john);
+    	save(jane);
+    	System.out.println(bAcc.getDebitCards().size());
+    	System.out.println(bAcc2.getDebitCards().size());
+    	john.deleteFromDB();
+    	jane.deleteFromDB();
+    	System.out.println(bAcc.getDebitCards().size());
+    	System.out.println(bAcc2.getDebitCards().size());
+    }
 }
