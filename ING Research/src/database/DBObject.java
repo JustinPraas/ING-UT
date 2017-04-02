bb4ef0b2554ded558c03fc2ef479962a4afb97d6
cjcr.andrei <cjcr.andrei@gmail.com>
2017-04-02 16:02:19 +0200

    Change getPrimaryKeyVal return type to Object in DBObject interface
    Some primary key values may not necessarily be of type String.

--- ING Research/src/database/DBObject.java
+++ ING Research/src/database/DBObject.java
@@ -2,7 +2,7 @@ package database;
 
 public interface DBObject {
 	public String getPrimaryKeyName();
-	public String getPrimaryKeyVal();
+	public Object getPrimaryKeyVal();
 	public String getClassName();
 	public void saveToDB();
 	public void deleteFromDB();
