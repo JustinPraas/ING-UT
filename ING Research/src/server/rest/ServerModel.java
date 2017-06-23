88c22b76a0ad76e47b7097d5262ffbebdaef9d8c
s1782215 <j.w.praas@student.utwente.nl>
2017-06-23 14:19:24 +0200

    Create a ServerModel

---
+++ ING Research/src/server/rest/ServerModel.java
@@ -0,0 +1,22 @@
+package server.rest;
+
+import java.util.HashMap;
+
+public class ServerModel {// Extension 2
+	
+	// previousPinAttempts[pincard, No. attempts]
+	private HashMap<String, Integer> previousPinAttempts = new HashMap<>();
+
+	public void increaseInvalidPinAttempt(String cardNumber) {
+		if (previousPinAttempts.containsKey(cardNumber)) {
+			previousPinAttempts.put(cardNumber, 1);
+		} else {
+			previousPinAttempts.put(cardNumber, previousPinAttempts.get(cardNumber) + 1);
+		}		
+	}
+	
+	public HashMap<String, Integer> getPreviousPinAttempts() {
+		return previousPinAttempts;
+	}
+
+}
