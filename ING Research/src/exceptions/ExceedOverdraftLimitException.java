6e1ce832f111ebe4233a597223bfd73b13527dff
s1782215 <j.w.praas@student.utwente.nl>
2017-06-24 18:39:02 +0200

    Add ExceedOverdraftLimitException and add javadoc to it

---
+++ ING Research/src/exceptions/ExceedOverdraftLimitException.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+/**
+ * Thrown when a user wants to pay/transfer money, but the amount exceeds the overdraft limit.
+ * @author Justin Praas
+ */
+public class ExceedOverdraftLimitException extends Exception {
+
+	private static final long serialVersionUID = 5436165626961779364L;
+	private double overdraftLimit;
+	
+	public ExceedOverdraftLimitException(double overdraftLimit) {
+		this.overdraftLimit = overdraftLimit;
+	}
+
+	public String toString() {
+		return "Can not compleet payment/transfer: exceeding overdraft limit of " + overdraftLimit;
+	}
+}
