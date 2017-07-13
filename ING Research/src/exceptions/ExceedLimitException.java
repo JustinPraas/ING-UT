d28271b2e0a5611e24b0aedcb425ac6c28514846
s1782215 <j.w.praas@student.utwente.nl>
2017-07-13 10:50:32 +0200

    Refactor ExceedLimitException

--- ING Research/src/exceptions/ExceedLimitException.java
+++ ING Research/src/exceptions/ExceedLimitException.java
@@ -4,16 +4,22 @@ package exceptions;
  * Thrown when a user wants to pay/transfer money, but the amount exceeds the overdraft limit.
  * @author Justin Praas
  */
-public class ExceedOverdraftLimitException extends Exception {
+public class ExceedLimitException extends Exception {
 
 	private static final long serialVersionUID = 5436165626961779364L;
-	private double overdraftLimit;
+	private double amount;
+	private String IBAN;
+	private LimitType limitType;
 	
-	public ExceedOverdraftLimitException(double overdraftLimit) {
-		this.overdraftLimit = overdraftLimit;
+	public enum LimitType {OVERDRAFT_LIMIT, DEBITCARD_LIMIT, WEEKLY_ACCOUNT_LIMIT};
+	
+	public ExceedLimitException(double amount, String IBAN, LimitType limitType) {
+		this.amount = amount;
+		this.IBAN = IBAN;
+		this.limitType = limitType;		
 	}
 
 	public String toString() {
-		return "Can not complete payment/transfer: exceeding overdraft limit of " + overdraftLimit + ".";
+		return "Can not complete transaction: exceeding " + limitType.name() + " with amount: " + amount + ".";
 	}
 }
