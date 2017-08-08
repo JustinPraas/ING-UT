3d0189b0963b623446fa8777c58b225a5e049245
s1782215 <j.w.praas@student.utwente.nl>
2017-08-08 15:27:09 +0200

    Implement the BankSystemValue enum

---
+++ ING Research/src/server/rest/BankSystemValue.java
@@ -0,0 +1,27 @@
+package server.rest;
+
+public enum BankSystemValue {
+
+	CREDIT_CARD_MONTHLY_FEE		(0.0),
+	CREDIT_CARD_DEFAULT_CREDIT	(0.0),
+	CARD_EXPIRATION_LENGTH		(5.0),
+	NEW_CARD_COST				(7.5),
+	CARD_USAGE_ATTEMPTS			(3.0),
+	MAX_OVERDRAFT_LIMIT			(5000.0),
+	INTEREST_RATE_1				(0.15),
+	INTEREST_RATE_2				(0.15),
+	INTEREST_RATE_3				(0.20),
+	OVERDRAFT_INTEREST_RATE		(10.0),
+	DAILY_WITHDRAW_LIMIT		(250.0), 
+	WEEKLY_TRANSFER_LIMIT		(2500.0);
+	
+	private final double amount;
+	
+    BankSystemValue(double amount) {
+        this.amount = amount;
+    }
+
+	public double getAmount() {
+		return amount;
+	}
+}
