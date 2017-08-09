34e50b80cb67c8906e2f16d8fd2f56ee32f3de20
s1782215 <j.w.praas@student.utwente.nl>
2017-08-09 12:30:31 +0200

    Simplify the TimeOperator class HEAVILY. Also makes it more scalable!
    Used TimeEvents in a better way to have future events stored AND
    previous events (with the 'executed' field in TimeEvent)

--- ING Research/src/server/rest/TimeOperator.java
+++ ING Research/src/server/rest/TimeOperator.java
@@ -1,5 +1,11 @@
 package server.rest;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,11 +14,13 @@ import accounts.BankAccount;
 import database.DataManager;
 import database.SQLiteDB;
 import exceptions.ObjectDoesNotExistException;
+import logging.Logger;
 
 public class TimeOperator extends Thread {
 	
 	public TimeOperator() {
 		start();
+		BankSystemValue.init();
 	}
 
 	/**
@@ -23,12 +31,10 @@ public class TimeOperator extends Thread {
 		while (true) {
 			Calendar c = ServerModel.getServerCalendar();
 
-			// Events on the first day of the month
-			if (c.get(Calendar.DATE) == 1) {
-				SQLiteDB.connectionLock.lock();
-				updateTransferLimitsIfNeeded(c);
-				SQLiteDB.connectionLock.unlock();
-			}
+			// Update events on a daily basis
+			SQLiteDB.connectionLock.lock();
+			updateSystem(c);
+			SQLiteDB.connectionLock.unlock();
 			
 			try {
 				// Sleep for a day
@@ -38,63 +44,101 @@ public class TimeOperator extends Thread {
 			}	
 		}
 	}
-
-	/**
-	 * @param newlySimulatedDays2 
-	 * 
-	 */
+	
 	public static void simulateDays(int newlySimulatedDays) {
 		SQLiteDB.connectionLock.lock();
 		Calendar serverCalendar = ServerModel.getServerCalendar();
 		for (int i = 1; i <= newlySimulatedDays; i++) {
-			serverCalendar.add(Calendar.DATE, 1);
-			if (serverCalendar.get(Calendar.DATE) == 1 && !updatedTransferLimitsThisMonth(serverCalendar)) {
-				updateTransferLimitsIfNeeded(serverCalendar);
-				break;
-			}			
+			serverCalendar.add(Calendar.DATE, 1);		
+			updateSystem(serverCalendar);
 		}	
 		SQLiteDB.connectionLock.unlock();
 	}
-
-	/**
-	 * Updates the transfer limits for all pending bank accounts. 
-	 */
-	private static void updateTransferLimitsIfNeeded(Calendar serverCalendar) {	
-		if (serverCalendar.get(Calendar.DATE) == 1 && !updatedTransferLimitsThisMonth(serverCalendar)) {
-			System.out.println("Updating transfer limits: " + serverCalendar.getTime().toString());
-			HashMap<String, Double> updatedTransferLimitMap = ServerDataHandler.getUpdatedTransferLimitMap();
-			for (Map.Entry<String, Double> entry : updatedTransferLimitMap.entrySet()) {
-				try {
-					BankAccount bankAccount = (BankAccount) DataManager.getObjectByPrimaryKey(BankAccount.CLASSNAME, entry.getKey());
-					bankAccount.setTransferLimit(entry.getValue());
-					bankAccount.saveToDB();
-				} catch (ObjectDoesNotExistException e) {
-					// Bank account does not exist.. do nothing with the entry
-				}
-			}
-			ServerDataHandler.setUpdatedTransferLimitMap(new HashMap<>());
-			new TimeEvent(TimeEvent.UPDATE_TRANSFER_LIMITS, serverCalendar.getTimeInMillis(), 
-					"Updated the transfer limits on " + serverCalendar.getTime().toString()).saveToDB();
-		}			
-	}
-
-	private static boolean updatedTransferLimitsThisMonth(Calendar serverCalendar) {
-		Calendar lastExecution = getTimestampOf(TimeEvent.UPDATE_TRANSFER_LIMITS);
-		if (lastExecution != null) {
-			return lastExecution.get(Calendar.DATE) == 1 && lastExecution.get(Calendar.MONTH) == serverCalendar.get(Calendar.MONTH) && lastExecution.get(Calendar.YEAR) == serverCalendar.get(Calendar.YEAR);
-		} else {
-			return false;
-		}		
-	}
-
-	private static Calendar getTimestampOf(String settingName) {
-		TimeEvent timeEvent;
+	
+	private static void updateSystem(Calendar c) {
+		String date = c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1) + "-" + c.get(Calendar.DATE);
+		long startMillis = 0;
 		try {
-			timeEvent = (TimeEvent) DataManager.getObjectByPrimaryKey(TimeEvent.CLASSNAME, settingName);
-			return timeEvent.getCalendar();
-		} catch (ObjectDoesNotExistException e) {
-			return null;
+			startMillis = Logger.parseDateToMillis(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return;
 		}
+		long endMillis = startMillis + 1000 * 3600 * 24;
+		
+		ArrayList<TimeEvent> todaysUnfinishedEvents = getTodaysEvents(startMillis, endMillis);
+		
+		for (TimeEvent t : todaysUnfinishedEvents) {
+			switch (t.getName()) {
+			case "BANK_SYSTEM_VALUE_UPDATE":
+				updateBankSystemValue(t);
+				break;
+			case "TRANSFER_LIMIT_UPDATE":
+				updateTransferLimit(t);
+				break;
+			}
+		}
+	}
+	
+	private static void updateBankSystemValue(TimeEvent t) {
+		String[] descriptionArray = t.getDescription().split(":");
+		String key = descriptionArray[0];
+		String value = descriptionArray[1];
+		BankSystemValue.updateBankSystemValue(key, value);
+		t.setExecuted(true);
+		t.saveToDB();
+	}
+	
+	private static void updateTransferLimit(TimeEvent t) {
+		HashMap<String, Double> updatedTransferLimitMap = ServerDataHandler.getUpdatedTransferLimitMap();
+		for (Map.Entry<String, Double> entry : updatedTransferLimitMap.entrySet()) {
+			try {
+				BankAccount bankAccount = (BankAccount) DataManager.getObjectByPrimaryKey(BankAccount.CLASSNAME, entry.getKey());
+				bankAccount.setTransferLimit(entry.getValue());
+				bankAccount.saveToDB();
+			} catch (ObjectDoesNotExistException e) {
+				// Bank account does not exist.. do nothing with the entry
+			}
+		}
+		
+		// Update events
+		t.setExecuted(true);
+		t.saveToDB();
+		
+		TimeEvent nextMonthUpdate = new TimeEvent();
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(t.getTimestamp());
+		c.add(Calendar.MONTH, 1);
+		nextMonthUpdate.setName(t.getName());
+		nextMonthUpdate.setDescription(t.getDescription());
+		nextMonthUpdate.setTimestamp(c.getTimeInMillis());
+		nextMonthUpdate.setExecuted(false);
+		nextMonthUpdate.saveToDB();
+	}
+
+	private static ArrayList<TimeEvent> getTodaysEvents(long startMillis, long endMillis) {
+		ArrayList<TimeEvent> result = new ArrayList<>();
+    	Connection c = SQLiteDB.openConnection();	
+		ResultSet rs;
+		try {
+			PreparedStatement s = c.prepareStatement("SELECT * FROM timeevents WHERE timestamp >= ? AND timestamp < ? AND executed = 0");
+			s.setLong(1, startMillis);
+			s.setLong(2, endMillis);
+			rs = s.executeQuery();
+			while (rs.next()) {
+				TimeEvent timeEvent = new TimeEvent();
+				timeEvent.setID(rs.getInt("ID"));
+				timeEvent.setName(rs.getString("name"));
+				timeEvent.setTimestamp(Long.parseLong(rs.getString("timestamp")));
+				timeEvent.setDescription(rs.getString("description"));
+				timeEvent.setExecuted(rs.getBoolean("executed"));
+				result.add(timeEvent);
+			}
+		} catch (NumberFormatException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;	
 	}
 
 }
